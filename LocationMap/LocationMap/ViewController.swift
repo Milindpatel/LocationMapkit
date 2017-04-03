646bae54840273a21abf7d9e0874ceb9c7e24e40
Milindpatel <milindpatel3959@gmail.com>
2017-04-03 01:43:54 -0400

    modified span ,and add one member in viewdidLoad
    All works fine.All done

--- LocationMap/LocationMap/ViewController.swift
+++ LocationMap/LocationMap/ViewController.swift
@@ -20,12 +20,14 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let location = locations[0]
         
-        let span:MKCoordinateSpan = MKCoordinateSpanMake(location.coordinate.latitude, location.coordinate.longitude)
+        let span:MKCoordinateSpan = MKCoordinateSpanMake(0.01, 0.01)
         
         let myLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
         
         let region:MKCoordinateRegion = MKCoordinateRegionMake(myLocation, span)
         map.setRegion(region, animated: true)
+        
+        
         
         self.map.showsUserLocation = true
     }
@@ -35,6 +37,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         // Do any additional setup after loading the view, typically from a nib.
         manager.delegate = self
         manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
         manager.startUpdatingLocation()
         
     }
