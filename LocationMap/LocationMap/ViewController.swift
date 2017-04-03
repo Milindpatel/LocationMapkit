22db507e8a941d6db932c5cbf0542c203dbc72d7
Milindpatel <milindpatel3959@gmail.com>
2017-04-03 01:17:42 -0400

    connect outlet map to ViewController.swift file and add some variable related to map

--- LocationMap/LocationMap/ViewController.swift
+++ LocationMap/LocationMap/ViewController.swift
@@ -12,11 +12,31 @@ import CoreLocation
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
     
+    //Map
+    @IBOutlet var map: MKMapView!
     
+    let manager = CLLocationManager()
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[0]
+        
+        let span:MKCoordinateSpan = MKCoordinateSpanMake(location.coordinate.latitude, location.coordinate.longitude)
+        
+        let myLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+        
+        let region:MKCoordinateRegion = MKCoordinateRegionMake(myLocation, span)
+        map.setRegion(region, animated: true)
+        
+        self.map.showsUserLocation = true
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.startUpdatingLocation()
+        
     }
 
     override func didReceiveMemoryWarning() {
